44a59fbe9b9228fde5c82d4867d228ac232d13cf
Gustavo <gugapmotta@gmail.com>
2025-11-14 02:43:35 -0300

    Amanhã eu continuo, to cansadito.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("OFFICEFLOW")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Gerenciador de ativos de TI para pequenas e médias empresas."
 )
