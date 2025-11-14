1c1f11799fadb9e29d9fce5519ac486d3fb76433
Gustavo <gugapmotta@gmail.com>
2025-11-14 16:06:12 -0300

    Teste inicial

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,23 @@
+from supabase import create_client
 import streamlit as st
+import os
 
-st.title("OFFICEFLOW")
-st.write(
-    "Gerenciador de ativos de TI para pequenas e médias empresas."
-)
+SUPABASE_URL = st.secrets["SUPABASE_URL"]
+SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
+
+supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
+
+name = st.text_input("Nome")
+category = st.number_input("Categoria ID", step=1)
+description = st.text_area("Descrição")
+
+if st.button("Salvar"):
+    data = {
+        "name": name,
+        "category_id": category,
+        "description": description
+    }
+
+    result = supabase.table("assets").insert(data).execute()
+    st.success("Asset criado!")
+    st.json(result.data)
